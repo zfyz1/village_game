a85ac28b50ebccd248441f557bd7ef8ca2d9eae7
550954155 <550954155@qq.com>
2026-06-15 15:42:50 +0800

    修改：在线玩家列表弹窗显示

--- frontend/src/views/LobbyView.vue
+++ frontend/src/views/LobbyView.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
+import {ref} from "vue";
+
 import { Lobby } from "../composables/useLobby"
 import OnlinePlayerList from "../components/OnlinePlayerList.vue";
 
 
 const { current_nickname, current_id} = Lobby()
+
+const showPlayerListDialog = ref(false)
 </script>
 
 <template>
@@ -13,8 +17,20 @@ const { current_nickname, current_id} = Lobby()
       <p>欢迎回来，用户{{ current_nickname }}！你的唯一id是：{{ current_id }}</p>
     </div>
 
-    <div class="player-list-container">
-      <OnlinePlayerList />
+    <div>
+      <button @click="showPlayerListDialog = true"> 显示在线玩家 </button>
+    </div>
+
+    <!-- 在线列表弹窗 -->
+    <div v-if="showPlayerListDialog" class="Dialog">
+      <div>
+        <h3> 在线玩家 </h3>
+        <button @click="showPlayerListDialog = false"> 关闭 </button>
+      </div>
+      <div>
+        <OnlinePlayerList/>
+      </div>
     </div>
   </div>
+
 </template>
