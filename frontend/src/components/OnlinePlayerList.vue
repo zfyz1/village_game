b5582841eba8dc6e18222aca7473df1c3855df2f
550954155 <550954155@qq.com>
2026-06-15 16:05:51 +0800

    更新：实时在线玩家列表

--- frontend/src/components/OnlinePlayerList.vue
+++ frontend/src/components/OnlinePlayerList.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, onMounted } from 'vue'
+import {ref, onMounted, onUnmounted} from 'vue'
 
 import { socket } from '../utils/socket'
 
@@ -15,6 +15,12 @@ interface PlayersListData {
   online_count: number
 }
 
+interface NewPlayer{
+  player_id: string
+  nickname: string
+  online_count: number
+}
+
 // 2. 响应式数据
 const playerList = ref<Player[]>([])  // 存储玩家数组
 const onlineCount = ref(0)
@@ -25,27 +31,43 @@ const handlePlayersList = (data : PlayersListData) => {
   onlineCount.value = data.online_count
 }
 
+const handlePlayerJoined = (data : NewPlayer) => {
+  console.log('新玩家加入：',data)
+  // 刷新玩家列表
+  socket.emit('get_players')
+}
 
+const handlePlayerLeft = (data : NewPlayer) => {
+  console.log('玩家离开：',data)
+  socket.emit('get_players')
+}
 
 // 4. 声明周期钩子
 onMounted( ()=> {
-
-  if(!socket.connected){
+  if(!socket.active){
     socket.connect()
   }
 
   socket.on('players_list', handlePlayersList)
+  socket.on('player_joined',handlePlayerJoined)
+  socket.on('player_left',handlePlayerLeft)
   socket.emit('get_players')
 
   }
 )
+
+onUnmounted(() => {
+  socket.off('players_list', handlePlayersList)
+  socket.off('player_joined', handlePlayerJoined)
+  socket.off('player_left',handlePlayerLeft)
+})
 
 
 </script>
 
 <template>
   <div class="online-player-list">
-    <span>当前在线人数 {{  }}</span>
+    <span>当前在线人数 {{ onlineCount }}</span>
     <div>
       <div class="table"
            v-for="player in playerList"
